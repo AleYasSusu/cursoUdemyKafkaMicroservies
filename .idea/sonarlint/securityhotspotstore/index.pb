67e284e2e0f6fb57aca902b345ae4c506d7d2644
Alessandro <alessandro.souza@edu.pucrs.br>
2023-05-28 22:05:46 -0300

    imolementado servidor de configuracao

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,13 @@
 
 •
 eemployee-service/src/main/java/com/aledev/employeeservice/controller/impl/EmployeeControllerImpl.java,1/6/1680887fa0e42956bc518260233d5f6ed0d95a09
+‘
+adepartament-service/src/main/java/com/aledev/departamentservice/DepartmentServiceApplication.java,4/e/4ea5b5a93d86bebee9f8f1276dd985e63ab054e2
+€
+Pconfig-server/src/main/java/com/aledev/configserver/ConfigServerApplication.java,8/b/8b4aee144294ba124b2b454bbb1ab5bad42cda3b
+K
+departament-service/pom.xml,d/1/d1d077e27e843c02829aafb495e2ba9f0e59645a
+H
+employee-service/pom.xml,f/d/fd812e5ef9218413b1f2b5c11155b42a1059175f
+E
+config-server/pom.xml,a/2/a2f9618c8ad821b143a07a29a9805fcb0b2df470
--- departament-service/src/main/java/com/aledev/departamentservice/controller/DepartmentController.java
+++ departament-service/src/main/java/com/aledev/departamentservice/controller/DepartmentController.java
@@ -2,13 +2,13 @@ package com.aledev.departamentservice.controller;
 
 import com.aledev.departamentservice.dto.DepartmentDto;
 import com.aledev.departamentservice.service.DepartmentService;
-import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -17,14 +17,14 @@ public class DepartmentController {
     private final DepartmentService departmentService;
     @PostMapping
     public ResponseEntity< DepartmentDto> createDepartment( @RequestBody DepartmentDto departmentDto) {
-        DepartmentDto userSaved = departmentService.createNewDepartment(departmentDto);
-        return new ResponseEntity<>(userSaved, HttpStatus.CREATED);
+        DepartmentDto departmentSaved = departmentService.createNewDepartment(departmentDto);
+        return new ResponseEntity<>(departmentSaved, HttpStatus.CREATED);
     }
 
     @GetMapping("/{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String department) {
-        DepartmentDto userDto = departmentService.getDepartment(department);
-        return new ResponseEntity<>(userDto, HttpStatus.OK);
+        DepartmentDto departmentDto = departmentService.getDepartment(department);
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
     }
 
 }
